18ea0a20fc93bde8f7a11246b090a59846556463
zhuanzhuyihao <zhuanzhuyihao@163.com>
2020-08-22 13:51:36 +0800

    完成 button group

---
+++ src/button-group.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="v-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.v-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .v-button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
