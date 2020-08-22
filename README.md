41e4bd6ceac9fe8ee517ca971802eac2ce9ce865
zhuanzhuyihao <zhuanzhuyihao@163.com>
2020-08-22 08:04:24 +0800

    ignore .cache  dist

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 https://github.com/zhuanzhuyihao/vue-ui.git
+
+
+安装 parcel 后重新处理文件
+
+运行parcel
+```js
+./node_modules/.bin/parcel index.html  --no-cache
+npx parcel index.html
+```
---
+++ src/button.vue
@@ -0,0 +1,36 @@
+// 单文件组件
+
+<template>
+  <div>
+    <button class="v-button">hi</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<style lang="scss"  scoped>
+.v-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--horder-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
