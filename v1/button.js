0f9bd9d278779b842a93a54f14fe7c98956fb67f
zhuanzhuyihao <zhuanzhuyihao@163.com>
2020-10-18 22:56:48 +0800

    清理代码

--- v1/button.js
+++
@@ -1,7 +0,0 @@
-Vue.component('v-button', { template: `<button class="v-button">hi</button>` })
-// 使用Vue.component声明一个组件
-Vue.component('g-button', {
-  template: `
-    <button>Hi</button>
-  `,
-})
