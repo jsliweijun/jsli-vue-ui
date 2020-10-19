b58a60f149f1630eebf027e0e66bca2ca0c82efa
zhuanzhuyihao <zhuanzhuyihao@163.com>
2020-10-19 09:11:18 +0800

    实现 gutter 功能的开发

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="col" :class="[`col-${span}  offset-${offset}`]">
-    <slot></slot>
+  <div
+    class="col"
+    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{ paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px' }"
+  >
+    <div style="border: 1px solid green; height: 100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -13,9 +19,15 @@ export default {
     offset: {
       type: [Number, String],
     },
+    // 给出警告，使用 data，computed 来接收父组件在mounted时给子组件的传参
+    // gutter: {
+    //   type: [Number, String],
+    // },
   },
   data() {
-    return {};
+    return {
+      gutter: 0,
+    };
   },
   components: {},
 };
@@ -24,9 +36,9 @@ export default {
 <style lang="scss" scoped>
 .col {
   height: 100px;
-  background: grey;
+  // background: grey;
   width: 50%;
-  border: 1px solid red;
+  // border: 1px solid red;
   // 用 scss 变量进行测试 24 份的宽度
   $class-prefix: col-;
   @for $n from 1 through 24 {
--- src/row.vue
+++ src/row.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="row">
+  <div
+    class="row"
+    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
+  >
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    gutter: {
+      type: [Number, String],
+    },
+  },
   data() {
     return {};
   },
   components: {},
+  created() {
+    // 没有子组件
+  },
+  mounted() {
+    // 有子组件
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter;
+    });
+  },
 };
 </script>
 
